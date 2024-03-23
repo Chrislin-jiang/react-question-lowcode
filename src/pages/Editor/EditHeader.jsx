e220aba716a1811626bb0e43e68d8d18d29492ae
gongjianglin666 <2116398301@qq.com>
2024-03-23 17:14:34 +0800

    1

---
+++ src/pages/Editor/EditHeader.jsx
@@ -0,0 +1,8 @@
+
+// 编辑器头部
+const EditHeader = () => {
+
+  return <div>Header</div>
+}
+
+export default EditHeader
--- src/pages/Editor/index.jsx
+++ src/pages/Editor/index.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
+// import { useParams } from 'react-router-dom'
+// import { useDispatch } from 'react-redux'
+// import { useTitle } from 'ahooks'
+// import { changeSelectedId } from '../../../store/componentsReducer'
+// import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
+// import useGetPageInfo from '../../../hooks/useGetPageInfo'
+import EditHeader from './EditHeader'
+// import EditCanvas from './EditCanvas'
+// import LeftPanel from './LeftPanel'
+// import RightPanel from './RightPanel'
+import styles from './index.module.scss'
 
-export default function Editor() {
-    return (
-        <div>Editor</div>
-    )
+const Edit = () => {
+  // const { id = '' } = useParams()
+  // const dispatch = useDispatch()
+
+  // const { loading } = useLoadQuestionData()
+
+  // function clearSelectedId() {
+  //   dispatch(changeSelectedId(''))
+  // }
+
+  // 修改标题
+  // const { title } = useGetPageInfo()
+  // useTitle(`问卷编辑 - ${title}`)
+
+  return (
+    <div className={styles.container}>
+      <EditHeader />
+      <div className={styles['content-wrapper']}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            {/* <LeftPanel /> */}
+            LeftPanel
+          </div>
+          <div
+            className={styles.main}
+          // onClick={clearSelectedId}
+          >
+            <div className={styles['canvas-wrapper']}>
+              {/* <EditCanvas /> */}
+              EditCanvas
+            </div>
+          </div>
+          <div className={styles.right}>
+            Right
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Edit
